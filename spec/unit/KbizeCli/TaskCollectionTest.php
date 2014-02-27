e98545230c21cd0fc77978a895e8f1dfc356aec0
Danilo <danilo@anycode.it>
2014-02-27 08:15:46 +0100

    added class TaskCollection with the feature of filters his child according to an array of filters

---
+++ spec/unit/KbizeCli/TaskCollectionTest.php
@@ -0,0 +1,101 @@
+<?php
+namespace KbizeCli;
+
+class TaskCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreation()
+    {
+        $tasks = $this->sampleTasks();
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals($tasks, $taskCollection->tasks());
+    }
+
+    public function testFilterCollectionWithLowercaseFilterAndLowercaseValue()
+    {
+        $tasks = $this->sampleTasks();
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals(
+            [['Lorem', 'ipsum', 'dolor', 'sit', 'amet']],
+            $taskCollection->filter(['ipsum'])
+        );
+    }
+
+    public function testFilterCollectionWithLowercaseFilterAndUppercaseValue()
+    {
+        $tasks = $this->sampleTasks();
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals(
+            [['Lorem', 'ipsum', 'dolor', 'sit', 'amet']],
+            $taskCollection->filter(['lorem'])
+        );
+    }
+
+    public function testFilterCollectionWithMultipleExistingFilters()
+    {
+        $tasks = $this->sampleTasks();
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals(
+            [['Cum', 'sociis', 'natoque', 'penatibus', 'et', 'magnis']],
+            $taskCollection->filter(['sociis', 'pena'])
+        );
+    }
+
+    public function testFilterCollectionWithMultipleNotAllExistingFilters()
+    {
+        $tasks = $this->sampleTasks();
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals(
+            [],
+            $taskCollection->filter(['sociis', 'not-exists'])
+        );
+    }
+
+    public function testFilterCollectionWithMultipleNotAllExistingFiltersWithOrStrategy()
+    {
+        $tasks = $this->sampleTasks();
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals(
+            [['Cum', 'sociis', 'natoque', 'penatibus', 'et', 'magnis']],
+            $taskCollection->filter(['sociis', 'not-exists'], false)
+        );
+    }
+
+    public function testFilterCollectionWithFilterOnSpecificKey()
+    {
+        $t1 = [
+            'title' => 'foo',
+            'description' => 'bar',
+        ];
+
+        $t2 = [
+            'title' => 'bar',
+            'description' => 'foo',
+        ];
+
+        $tasks = [$t1, $t2];
+
+        $taskCollection = new TaskCollection($tasks);
+
+        $this->assertEquals(
+            [$t1],
+            $taskCollection->filter(['title=foo'])
+        );
+    }
+
+    private function sampleTasks()
+    {
+        return [
+            ['Lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+            ['Cum', 'sociis', 'natoque', 'penatibus', 'et', 'magnis'],
+            ['Sed', 'ultricies', 'fringilla', 'tristique'],
+            ['Morbi', 'interdum', 'felis', 'a augue', 'bibendum', 'suscipit'],
+            [new \stdClass(), ['foo', 'bar']],
+        ];
+    }
+}
---
+++ src/KbizeCli/TaskCollection.php
@@ -0,0 +1,111 @@
+<?php
+namespace KbizeCli;
+
+class TaskCollection
+{
+    private $tasks;
+
+    public function __construct(array $tasks = [])
+    {
+        $this->tasks = $tasks;
+    }
+
+    public function tasks()
+    {
+        return $this->tasks;
+    }
+
+    public function filter(array $filters = [], $useAnd = true)
+    {
+        $collectedTasks = [];
+
+        if ($useAnd) {
+            $strategy = new AndMatcher();
+        } else {
+            $strategy = new OrMatcher();
+        }
+
+        foreach ($this->tasks as $task) {
+            if ($strategy->match($task, $filters)) {
+                $collectedTasks[] = $task;
+            }
+        }
+
+        return $collectedTasks;
+    }
+}
+
+class AndMatcher extends Matcher
+{
+    public function match($collection, $filters)
+    {
+        $theCollectionMatch = true;
+
+        foreach ($filters as $filter) {
+            $theCollectionMatch &= $this->collectionMatch($collection, $filter);
+        }
+
+        return $theCollectionMatch;
+    }
+}
+
+class OrMatcher extends Matcher
+{
+    public function match($collection, $filters)
+    {
+        $theCollectionMatch = false;
+
+        foreach ($filters as $filter) {
+            $theCollectionMatch |= $this->collectionMatch($collection, $filter);
+        }
+
+        return $theCollectionMatch;
+    }
+}
+
+abstract class Matcher
+{
+    abstract function match($collection, $filters);
+
+    protected function collectionMatch($collection, $filter)
+    {
+        $keyValueFilter = $this->keyValueFilter($filter);
+
+        foreach ($collection as $field => $fieldValue) {
+            if ($keyValueFilter['key'] && $field != $keyValueFilter['key']) {
+                continue;
+            }
+
+            if (is_array($fieldValue)) {
+                continue; //FIXME:!
+            }
+
+            if (is_object($fieldValue)) {
+                if (is_callable([$fieldValue, '__toString'])) {
+                    $fieldValue = $fieldValue->__toString();
+                } else {
+                    continue;
+                }
+            }
+
+            if (strpos(strtolower($fieldValue), strtolower($keyValueFilter['value'])) !== false) {
+                return true;
+            }
+        }
+    }
+
+    private function keyValueFilter($filter)
+    {
+        if (($pos = strpos($filter, '=')) !== false) {
+            return [
+                'key' => substr($filter, 0, $pos),
+                'value' => substr($filter, $pos + 1)
+            ];
+        }
+
+        return [
+            'key' => null,
+            'value' => $filter
+        ];
+    }
+}
