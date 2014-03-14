b98ebf5885f56dfec2ed3e680d8f2eff4a901470
Danilo <danilo@anycode.it>
2014-03-14 18:50:57 +0100

    removed unused method inerithed from copy from Client.php class

--- src/KbizeCli/Tests/Integration/Cli.php
+++ src/KbizeCli/Tests/Integration/Cli.php
@@ -2,36 +2,23 @@
 namespace KbizeCli\Tests\Integration;
 
 use Skel\DependencyInjection\Application;
-use KbizeCli\Tests\Integration\InputStream;
 use KbizeCli\Console\Helper\AlternateTableHelper;
 use Symfony\Component\Console\Helper\DialogHelper;
-use Symfony\Component\Console\Tester\CommandTester;
 
 class Cli
 {
-    private $inputStream;
-    private $application;
     private $command;
-    private $commandTester;
     private $options = [];
     private $inputs = [];
 
     public function __construct()
     {
-        $this->inputStream = $this->inputStream();
-        $this->application = $this->application();
         $this->options['--env'] = 'test';
         $this->options['--no-ansi'] = true;
     }
 
-    public function getInputStream()
-    {
-        return $this->inputStream;
-    }
-
     public function __destruct()
     {
-        $this->closeInputStream();
     }
 
     public function command($command)
@@ -44,13 +31,11 @@ class Cli
     public function input($input)
     {
         $this->inputs[] = $input;
-        fputs($this->inputStream, $input . PHP_EOL);
-        /* rewind($this->inputStream); */
     }
 
     public function hasData()
     {
-        return !feof($this->inputStream);
+        return count($this->inputs);
     }
 
     public function ensureIsEmpty()
@@ -59,14 +44,9 @@ class Cli
             return;
         }
 
-        $data = [];
-        while (!feof($this->inputStream)) {
-            $data[] = stream_get_line($this->inputStream, 4096, PHP_EOL);
-        }
-
         throw new \RuntimeException(
             'Client has input data that have not been read: ' .
-            var_export($data, true)
+            var_export($this->inputs, true)
         );
     }
 
@@ -100,6 +80,10 @@ class Cli
             $this->output = stream_get_contents($pipes[1]);
             $this->output .= stream_get_contents($pipes[2]);
 
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+
+            $returnValue = proc_close($process);
         }
 
         return $this->output;
@@ -107,43 +91,6 @@ class Cli
 
     public function getDisplay()
     {
-        /* return $this->commandTester->getDisplay(); */
         return $this->output;
-    }
-
-    private function application()
-    {
-        $application = new Application('KbizeCli');
-        $helperSet = $application->getHelperSet();
-        $helperSet->set(new AlternateTableHelper());
-        $application->setHelperSet($helperSet);
-
-        return $application;
-    }
-
-    private function inputStream()
-    {
-        $this->registerProtocol();
-        $stream = fopen("cli://inputStream", "r+", false);
-        stream_set_blocking($stream, true);
-
-        return $stream;
-    }
-
-    private function closeInputStream()
-    {
-        if (is_resource($this->inputStream)) {
-            fclose($this->inputStream);
-        }
-    }
-
-    //FIXME:! Move out
-    private function registerProtocol()
-    {
-        static $isRegistered = false;
-        if (!$isRegistered) {
-            stream_wrapper_register("cli", "\KbizeCli\Tests\Integration\InputStream") or die("Failed to register protocol");
-            $isRegistered = true;
-        }
     }
 }
